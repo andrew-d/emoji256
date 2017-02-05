6160b57baa18b75a2d55f95eb4e2337334de13a7
Andrew Dunham <andrew@du.nham.ca>
2017-02-04 21:13:43 -0800

    More Travis debugging

--- ci/script.sh
+++ ci/script.sh
@@ -13,12 +13,22 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    find target -type f
+    ls -alh ./target/$TARGET/debug || true
+    ls -alh ./target/$TARGET/release || true
 
     # We can't use 'cross run' because we need to pass a string to the binary
     # on stdin.  Just run the output manually.
-    echo 'Foobar' | ./target/$TARGET/debug/emoji256
-    echo 'Foobar' | ./target/$TARGET/release/emoji256
+    if [ ! -x ./target/$TARGET/debug/emoji256 ]; then
+        echo "Debug target doesn't exist!?"
+    else
+        echo 'Foobar' | ./target/$TARGET/debug/emoji256
+    fi
+
+    if [ ! -x ./target/$TARGET/release/emoji256 ]; then
+        echo "Release target doesn't exist!?"
+    else
+        echo 'Foobar' | ./target/$TARGET/release/emoji256
+    fi
 }
 
 # we don't run the "test phase" when doing deploys
